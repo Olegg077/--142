2ca41ea5d4cac1700b4320dc8a52915ac9df97a2
Балакина Екатерина Петровна <114657505+BalakinaEP@users.noreply.github.com>
2025-05-05 10:36:47 +0300

    Update main (1).cpp

--- main (1).cpp
+++ main (1).cpp
@@ -5,32 +5,44 @@ using namespace std;
 *@brief Переводит вес из граммов в унции
 *@return возвращает значение в унциях
 */
-double gramms_in_ounces(double gramms, double const ounces) {
-    return gramms / ounces;
-}
+double gramms_in_ounces(double gramms);
+
 /**
 *@brief Запрашивает ввод данных с клавиатуры
 *@return возвращает значение в граммах
 */
-double testinput() {
-    double gramms;
-    cout << "Введите вес в граммах: ";
-    cin >> gramms;
-    return gramms;
-}
+double testinput();
 
-int main() {
+int main() 
+{
     setlocale(LC_ALL, "ru");
-    const double ounces = 28.3;
-    
+       
     double gramms = testinput();
     if (gramms < 0) {
         cout << "Ошибка: вес не может быть отрицательным!" << endl;
         return 1;
-    }
+}
     
     cout << "Ввод выполнен корректно!" << endl;
     cout << "Вес в унциях: " << gramms_in_ounces(gramms, ounces);
     
     return 0;
 }
+
+double gramms_in_ounces(double gramms) {
+    const double ounces = 28.3;
+    return gramms / ounces;
+}
+
+ouble testinput() {
+    double gramms = 0;
+    cout << "Введите вес в граммах: ";
+    cin >> gramms;
+    if (cin.fail())
+    {
+        cout<<"Error value"<<endl;
+        abort();
+    }
+    return gramms;
+}
+
